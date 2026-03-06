77a4329b0c60f70c6dc3abf830e04e568c7df30c
Guillaume <guillaume@inniti.fr>
2026-03-06 17:01:49 +0100

    Retour Fin Exercice 7

--- tplocal/main.tf
+++ tplocal/main.tf
@@ -9,24 +9,53 @@ terraform {
 
 provider "docker" { host = "tcp://localhost:2375" }
 
+resource "docker_network" "private_network" {
+  name = "my_network"
+}
+
 resource "docker_image" "nginx" {
-  name         = var.image_docker
+  name         = var.docker_image_name
   keep_locally = true
 }
 
 resource "docker_container" "nginx" {
-  name  = var.nom_container
+  name  = var.docker_container_name
   image = docker_image.nginx.image_id
+
+  networks_advanced {
+    name    = docker_network.private_network.name
+    aliases = ["nginx"]
+  }
+
   ports {
-    internal = var.int_port
-    external = var.ext_port
+    internal = var.internal_port
+    external = var.external_port
   }
 
   provisioner "local-exec" {
-    command = "curl -s http://localhost:${var.ext_port} | grep 'Welcome'"
+    command = "curl -s http://localhost:${var.external_port} | grep 'Welcome'"
   }
 }
 
+resource "docker_image" "curl_image" {
+  name = "appropriate/curl"
+}
+
+resource "docker_container" "client" {
+  for_each = { for m in var.machines : m.name => m }
+
+  name  = "server-${each.value.name}"
+  image = docker_image.curl_image.image_id
+
+  command = ["sh", "-c", "curl -s http://nginx:80 && sleep 30"]
+
+  networks_advanced {
+    name = docker_network.private_network.name
+  }
+
+  depends_on = [docker_container.nginx]
+}
+
 output "nginx_container_id" {
-  value = docker_image.nginx.image_id
+  value = docker_container.nginx.id
 }
--- tplocal/variables.tf
+++ tplocal/variables.tf
@@ -1,20 +1,53 @@
-variable "image_docker" {
-  type        = string
-  description = "The id of the machine image (AMI) to use for the server."
-  default     = "nginx:latest"
+variable "docker_image_name" {
+  type    = string
+  default = "nginx:latest"
 }
-variable "nom_container" {
-  type        = string
-  description = "Nom du conteneur"
-  default     = "nginx-terraform"
+
+variable "docker_container_name" {
+  type    = string
+  default = "mon-serveur-web"
 }
-variable "ext_port" {
-  type        = number
-  description = "Port expose"
-  default     = 8080
+
+variable "external_port" {
+  type    = number
+  default = 8080
 }
-variable "int_port" {
-  type        = number
-  description = "Port interne"
-  default     = 80
+
+variable "internal_port" {
+  type    = number
+  default = 80
+}
+
+variable "client_count" {
+  type    = number
+  default = 3
+}
+
+variable "server_names" {
+  type    = list(string)
+  default = ["1", "2", "3"]
+}
+
+variable "machines" {
+  type = list(object({
+    name      = string
+    vcpu      = number
+    disk_size = number
+    region    = string
+  }))
+
+  validation {
+    condition     = alltrue([for m in var.machines : m.vcpu >= 2 && m.vcpu <= 64])
+    error_message = "vCPU doit être entre 2 et 64."
+  }
+
+  validation {
+    condition     = alltrue([for m in var.machines : m.disk_size >= 20])
+    error_message = "Le disque doit être >= 20 Go."
+  }
+
+  validation {
+    condition     = alltrue([for m in var.machines : contains(["eu-west-1", "us-east-1", "ap-southeast-1"], m.region)])
+    error_message = "Région non autorisée."
+  }
 }
