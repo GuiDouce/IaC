21e6d4dd3ba2612bbefcf33c345a387890e7fe0b
Guillaume <guillaume@inniti.fr>
2026-03-24 11:07:19 +0100

    Préparation pour l'exercice 4 tpaws

--- tpaws/main.tf
+++ tpaws/main.tf
@@ -8,29 +8,16 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "test"
-  secret_key = "test"
-  region     = "us-east-1"
-
-  # LocalStack endpoint configuration
-  s3_use_path_style           = true
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    s3  = "http://localhost:4566"
-    ec2 = "http://localhost:4566"
-  }
+  region = "us-east-1"
 }
 
 resource "aws_instance" "db" {
-  ami             = "ami-12345678"
-  instance_type   = var.instance_type
-  security_groups = [aws_security_group.web.name] # On peut réutiliser le même SG pour l'instant
-  key_name        = aws_key_pair.deployer.key_name
+  ami                    = "ami-0c101f26f147fa7fd"
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.web.id]
+  key_name               = aws_key_pair.deployer.key_name
 
   tags = {
-    Name = "var.db_instance_name"
+    Name = var.db_instance_name
   }
 }
