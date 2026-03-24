74285aaeffd9c1548270fd510ecd93d36fb8690c
Guillaume <guillaume@inniti.fr>
2026-03-24 12:00:24 +0100

    Exercice 6 tpaws

--- tpaws/s3.tf
+++ tpaws/s3.tf
@@ -19,3 +19,34 @@ resource "aws_s3_object" "demo_object" {
   source = "./test-file.txt"
   etag   = filemd5("./test-file.txt")
 }
+
+resource "aws_s3_bucket_public_access_block" "allow_public" {
+  bucket = aws_s3_bucket.demo_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read_policy" {
+  bucket = aws_s3_bucket.demo_bucket.id
+  depends_on = [aws_s3_bucket_public_access_block.allow_public]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.demo_bucket.arn}/*"
+      },
+    ]
+  })
+}
+
+output "s3_public_url" {
+  value = "https://${aws_s3_bucket.demo_bucket.bucket}.s3.amazonaws.com/hello-world.txt"
+}
